f963542fb41c141c0f0b3bc17dc3177d2b28e82e
Aldhi <aldhiwiduri@gmail.com>
2023-08-15 06:26:10 +0700

    update say hello with parameter name

--- go-lang-modules.go
+++ go-lang-modules.go
@@ -1,5 +1,5 @@
 package go_lang_modules
 
-func SayHello() string {
-	return "Hello world"
+func SayHello(name string) string {
+	return "Hello" + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Aldhi28/go-lang-modules
+module github.com/Aldhi28/go-lang-modules/v2
 
 go 1.20
